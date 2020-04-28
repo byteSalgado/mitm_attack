10623c45783d2c4700ef6eaec0463fe3c59b7c90
byteSalgado <63315825+byteSalgado@users.noreply.github.com>
2020-04-27 22:28:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,11 @@
 
 apt update
 apt upgrade
+pkg install bash*
 apt install mariadb
 mkdir -p  /data/data/com.termux/files/usr/etc/my.cnf.d
 mysql_install_db
 mysqld_safe -u root &
 chmod +x mitm.sh
+clear
+bash mitm.sh
